48eaf03062802194a675bfcd75dc28ba7af80d8a
Shadi38 <113238379+Shadi38@users.noreply.github.com>
2023-07-01 14:39:23 +0100

    finished

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
-
-import './App.css';
-import Child from './Child';
+import "./App.css";
+import Child from "./Child";
 import babyNames from "./babyNamesData.json";
-import Search from './Search';
-import BoyButton from './BoyButton';
-import GirlButton from './GirlButton';
-import AllButton from './AllButton';
+import Search from "./Search";
 
 function App() {
   return (
     <div className="App">
-      <div className='headerDiv'>
+      <div className="headerDiv">
         <Search names={babyNames} />
-        <BoyButton names={babyNames} /> 
-        <GirlButton names={babyNames} />
-        <AllButton names={babyNames} />
       </div>
       <Child names={babyNames} />
     </div>
--- src/Child.js
+++ src/Child.js
@@ -1,38 +1,78 @@
 import React, { useState } from "react";
 
 function Child(props) {
-  const [newData, setNewData] = useState(props.names.sort((a, b) => a.name.localeCompare(b.name)));
-//main buttons:
+  const [mainData, setMainData] = useState(
+    props.names.sort((a, b) => a.name.localeCompare(b.name))
+  );
+  const allData = props.names;
+  //main buttons:
   const [buttonVisibility, setButtonVisibility] = useState(
-    Array(newData.length).fill(true)
+    Array(mainData.length).fill(true)
   );
   const [deletedButtons, setDeletedButtons] = useState([]);
 
+  const handleClickBoy = () => {
+    // const copyNewData = [...newData];
+    const filteredData = allData.filter((item) => item.sex === "m");
+    setMainData(filteredData);
+  };
 
-//function for main buttons:
+  const handleClickGirl = () => {
+    const filteredData = allData.filter((item) => item.sex === "f");
+    setMainData(filteredData);
+  };
+
+  const handleClickAll = () => {
+    setMainData(allData);
+  };
+
+  //function for main buttons:
   const handleClick = (index) => {
     const updatedVisibility = [...buttonVisibility];
     updatedVisibility[index] = false;
     setButtonVisibility(updatedVisibility);
 
-    const deletedButtonData = newData[index];
-    setDeletedButtons((prevDeletedButtons) => 
-      [...prevDeletedButtons, deletedButtonData]
-      )
+    const deletedButtonData = mainData[index];
+    setDeletedButtons((prevDeletedButtons) => [
+      ...prevDeletedButtons,
+      deletedButtonData,
+    ]);
   };
-//function for removing favourite buttons after clicking the button
-  const handleClickFavourite = (index) => { 
-const updatedDeletedButtons = [...deletedButtons];
-//we accses the deleted element by '[0]'
-//buttonToAdd  return the new array without the deleted button
-const buttonToAdd = updatedDeletedButtons.splice(index, 1)[0];
-setDeletedButtons(updatedDeletedButtons);
-//adding deleted button from favourite section to main list
-setNewData((prevData) => [...prevData, buttonToAdd]);
-setButtonVisibility((prevVisibility) => [...prevVisibility, true]);
-   }
+  //function for removing favourite buttons after clicking the button
+  const handleClickFavourite = (index) => {
+    const updatedDeletedButtons = [...deletedButtons];
+    //we accses the deleted element by '[0]'
+    //buttonToAdd  return the new array without the deleted button
+    const buttonToAdd = updatedDeletedButtons.splice(index, 1)[0];
+    setDeletedButtons(updatedDeletedButtons);
+    //adding deleted button from favourite section to main list
+    setMainData((prevData) => [...prevData, buttonToAdd]);
+    setButtonVisibility((prevVisibility) => [...prevVisibility, true]);
+  };
   return (
     <div>
+      <button
+        type="radio"
+        onClick={handleClickBoy}
+        style={{ background: "lightBlue" }}
+      >
+        boys
+      </button>
+      <button
+        type="radio"
+        onClick={handleClickGirl}
+        style={{ background: "pink" }}
+      >
+        girls
+      </button>
+
+      <button
+        type="radio"
+        onClick={handleClickAll}
+        style={{ background: "yellow" }}
+      >
+        all
+      </button>
       {/* bring deleted buttons into favourite list  */}
       {deletedButtons.length > 0 && (
         <div>
@@ -59,7 +99,7 @@ setButtonVisibility((prevVisibility) => [...prevVisibility, true]);
       <hr></hr>
       {/* deleting each button from the main list and assign the button visibility to each buttons */}
       <div>
-        {newData.map((data, index) => {
+        {mainData.map((data, index) => {
           return (
             <div className="namesDiv" key={index}>
               {buttonVisibility[index] && (
